0f0d49772cd761aa0debccd1f2056adcfd0b55f0
Jessica Brown <jessicakbrown1120@gmail.com>
2017-11-08 15:29:20 -0600

    authentication added

--- server/routes/charts.router.js
+++ server/routes/charts.router.js
@@ -15,8 +15,21 @@ router.get('/', function(req, res) {
                     console.log(queryError);
                     res.sendStatus(500);
                 } else {
-                    console.log('all case data', resultObj.rows);
+                    if(req.isAuthenticated()) {
+                        // send back user object from database
+                        console.log('logged in', req.user);
+                        var userInfo = {
+                          username : req.user.username,
+                          admin: req.user.admin     //shara added 
+                        }
+                        console.log('all case data', resultObj.rows);
                         res.send(resultObj.rows);
+                    } else {
+                        // failure best handled on the server. do redirect here.
+                        console.log('not logged in');
+                        // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+                        res.send(false);
+                      } //end Authentication if/else
                     }//end else
                 }) //end query
             } //end pool else
