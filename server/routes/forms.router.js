570a5aa797e2e9321ff796eab80272cfbc6df06e
Jessica Brown <jessicakbrown1120@gmail.com>
2017-11-09 14:25:50 -0600

    agency gets added to forms route

--- server/routes/forms.router.js
+++ server/routes/forms.router.js
@@ -26,5 +26,84 @@ router.get('/cities', function(req, res) {
       // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
       res.send(false);
     }
-  });
+  });//end cities Get call
+
+  router.get('/counties', function(req, res) {
+    console.log('In counties Get for intake');
+    // check if logged in
+    if (req.isAuthenticated()) {
+      pool.connect(function (conErr, client, done) {
+        if (conErr) {
+          res.sendStatus(500);
+        } else {
+          client.query('SELECT * FROM counties', function (queryErr, resultObj) {
+            done();
+            if (queryErr) {
+              res.sendStatus(500);
+            } else {
+              res.send(resultObj.rows);
+            }
+          });
+        }
+      })
+    } else {
+      // failure best handled on the server. do redirect here.
+      console.log('not logged in');
+      // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+      res.send(false);
+    }
+  }); //end counties get call
+
+  router.get('/lawEnforcement', function(req, res) {
+    console.log('In lawEnforcement Get for intake');
+    // check if logged in
+    if (req.isAuthenticated()) {
+      pool.connect(function (conErr, client, done) {
+        if (conErr) {
+          res.sendStatus(500);
+        } else {
+          client.query('SELECT * FROM law_enforcement', function (queryErr, resultObj) {
+            done();
+            if (queryErr) {
+              res.sendStatus(500);
+            } else {
+              res.send(resultObj.rows);
+            }
+          });
+        }
+      })
+    } else {
+      // failure best handled on the server. do redirect here.
+      console.log('not logged in');
+      // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+      res.send(false);
+    }
+  });// end law enforcement Get call
+
+  router.get('/schools', function(req, res) {
+    console.log('In schools Get for intake');
+    // check if logged in
+    if (req.isAuthenticated()) {
+      pool.connect(function (conErr, client, done) {
+        if (conErr) {
+          res.sendStatus(500);
+        } else {
+          client.query('SELECT * FROM schools', function (queryErr, resultObj) {
+            done();
+            if (queryErr) {
+              res.sendStatus(500);
+            } else {
+              res.send(resultObj.rows);
+            }
+          });
+        }
+      })
+    } else {
+      // failure best handled on the server. do redirect here.
+      console.log('not logged in');
+      // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+      res.send(false);
+    }
+  }); //end schools get call
+
 module.exports = router;
