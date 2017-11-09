8b60ebabe8fec71b7b5ac8cee461d00069f678e2
sharaseaman <shara.seaman@gmail.com>
2017-11-09 17:14:07 -0600

    delete

--- server/routes/manage.router.js
+++ server/routes/manage.router.js
@@ -26,4 +26,56 @@ router.get('/', function(req, res) {
       res.send(false);
     }
   });
+
+  //update admin rights
+  router.put('/', function (req, res) {
+    if (req.isAuthenticated()) {
+        pool.connect(function (connectionError, client, done) {
+            console.log('req.body ->', req.body);
+            console.log('this is req.body.username',req.body.username);
+            console.log('this is req.body.username',req.body.admin);
+            var userNameEdit = req.body.username;
+            var userAdminEdit = req.body.admin
+            if (connectionError) {
+                console.log(connectionError);
+                res.sendStatus(501);
+            } else {
+                var pQuery = 'UPDATE users SET admin=$1 WHERE username=$2';
+               var valueArray = [userAdminEdit, userNameEdit];
+                
+                client.query(pQuery, valueArray, function (queryError, resultObj) {
+                    done();
+                })
+            }
+        });
+    }
+    else {
+        console.log('not logged in');
+        res.send(false);
+    }
+});
+
+router.delete('/', function (req, res) {
+  if (req.isAuthenticated()) {
+      pool.connect(function (connectionError, client, done) {
+          console.log('req.body ->', req.body);
+          console.log('delete router req.body.username',req.body.username);
+          var userNameEdit = req.body.username;
+          if (connectionError) {
+              console.log(connectionError);
+              res.sendStatus(501);
+          } else {
+              var pQuery = 'DELETE FROM users WHERE username=$1';
+             var valueArray = [userNameEdit];
+              client.query(pQuery, valueArray, function (queryError, resultObj) {
+                  done();
+              })
+          }
+      });
+  }
+  else {
+      console.log('not logged in');
+      res.send(false);
+  }
+});
 module.exports = router;
