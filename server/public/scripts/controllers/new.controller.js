2386e74687352b249cbef11dd1523c3ede2190ff
sharaseaman <shara.seaman@gmail.com>
2017-11-09 14:37:50 -0600

    pdf bones work

--- server/public/scripts/controllers/new.controller.js
+++ server/public/scripts/controllers/new.controller.js
@@ -1,6 +1,18 @@
 myApp.controller('NewController', function (UserService) {
-    console.log('NewController created');
+    console.log('NewController as nwc created');
     var vm = this;
     vm.userService = UserService;
     vm.userObject = UserService.userObject;
+
+    //shara working on pdf below
+    var pdf = new jsPDF();
+    
+        vm.printPDF = function () {
+            console.log('test');
+    
+            pdf.text(10, 10, 'Name: ' + vm.caseIn);
+  
+            pdf.save();
+        }
+    //shara working on pdf above
   });
