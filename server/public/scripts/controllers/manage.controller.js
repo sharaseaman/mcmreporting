dfa56ba79f77f6d55f9d9944a81b3b5bdcbfc7c1
Alex Hand <alex.j.hand@gmail.com>
2017-11-09 14:16:51 -0600

    Fleshed out Manage Users page

--- server/public/scripts/controllers/manage.controller.js
+++ server/public/scripts/controllers/manage.controller.js
@@ -3,11 +3,29 @@ myApp.controller('ManageController', function (UserService) {
     var vm = this;
     vm.userService = UserService;
     vm.userObject = UserService.userObject;
+    
 
 
     //basic function to test manage route and get call
     vm.editUsers = function(){
-      UserService.editUsers();
+      UserService.editUsers().then(function() {
+        vm.users = UserService.users.list;
+        console.log('vm.users', vm.users);
+      })
+    }
+
+    vm.deleteUser = function(user) {
+      console.log('In deleteUser', user.username);
+      UserService.deleteUser(user).then(function () {
+        vm.editUsers();
+      })
+    }
+
+    vm.editPrivledge = function(user) {
+      console.log('In editPrivledge', user.username);
+      user.admin = !user.admin;
+      console.log('New admin value:', user.admin);
+      UserService.updatePrivledges(user);
     }
 
   });
