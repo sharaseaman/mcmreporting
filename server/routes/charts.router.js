8269769743b578359679984a7b59238792543d2f
Jessica Brown <jessicakbrown1120@gmail.com>
2017-11-11 10:10:42 -0600

    join table gets complete

--- server/routes/charts.router.js
+++ server/routes/charts.router.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var path = require('path');
 var pool = require('../modules/pool.js');
 
-// Handles Ajax request for user information if user is authenticated
+//get for all case data
 router.get('/', function(req, res) {
     console.log('In Charts Get for all case data');
     // check if logged in
@@ -28,4 +28,85 @@ router.get('/', function(req, res) {
       res.send(false);
     }
   });
+
+  //get for case_lawenforcement_denial join table
+router.get('/lawenforcementDenial', function(req, res) {
+  console.log('In Charts lawenforcementDenial Get');
+  // check if logged in
+  if (req.isAuthenticated()) {
+    pool.connect(function (conErr, client, done) {
+      if (conErr) {
+        res.sendStatus(500);
+      } else {
+        client.query('SELECT * FROM case_lawenforcement_denial', function (queryErr, resultObj) {
+          done();
+          if (queryErr) {
+            res.sendStatus(500);
+          } else {
+            res.send(resultObj.rows);
+          }
+        });
+      }
+    })
+  } else {
+    // failure best handled on the server. do redirect here.
+    console.log('not logged in');
+    // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+    res.send(false);
+  }
+});
+
+//get for case_race_ethnicity join table
+router.get('/raceEthnicity', function(req, res) {
+  console.log('In case_race_ethnicity Get');
+  // check if logged in
+  if (req.isAuthenticated()) {
+    pool.connect(function (conErr, client, done) {
+      if (conErr) {
+        res.sendStatus(500);
+      } else {
+        client.query('SELECT * FROM case_race_ethnicity', function (queryErr, resultObj) {
+          done();
+          if (queryErr) {
+            res.sendStatus(500);
+          } else {
+            res.send(resultObj.rows);
+          }
+        });
+      }
+    })
+  } else {
+    // failure best handled on the server. do redirect here.
+    console.log('not logged in');
+    // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+    res.send(false);
+  }
+});
+
+//get for case_vulnerabilities join table
+router.get('/vulnerabilities', function(req, res) {
+  console.log('In vulnerabilities Get');
+  // check if logged in
+  if (req.isAuthenticated()) {
+    pool.connect(function (conErr, client, done) {
+      if (conErr) {
+        res.sendStatus(500);
+      } else {
+        client.query('SELECT * FROM case_vulnerabilities', function (queryErr, resultObj) {
+          done();
+          if (queryErr) {
+            res.sendStatus(500);
+          } else {
+            res.send(resultObj.rows);
+          }
+        });
+      }
+    })
+  } else {
+    // failure best handled on the server. do redirect here.
+    console.log('not logged in');
+    // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+    res.send(false);
+  }
+});
 module.exports = router;
