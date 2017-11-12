544c39c1fb3c1009bfbe511a998951eae484d4b3
Kitty Ng <kittyng89@gmail.com>
2017-11-11 20:48:56 -0600

    added custom report filters, button and http request

--- server/public/scripts/controllers/core.controller.js
+++ server/public/scripts/controllers/core.controller.js
@@ -5,48 +5,55 @@ myApp.controller('CoreController', function (UserService, $scope) {
   vm.joinChartData = UserService.joinChartData;
   vm.selectedYear;
 
-  vm.getSelectedYear = function() { 
+  Chart.defaults.scale.ticks.beginAtZero = true;
+
+  vm.getSelectedYear = function () {
+
     if (vm.selectedYear !== undefined) {
       selectedYear = vm.selectedYear;
       UserService.updateChartYear(selectedYear);
-        //User Selected Year Charts 
-        vm.userCaseTypeLabels = UserService.userCaseTypeLabels;
-        vm.userFilteredCases = UserService.userFilteredCases;
-        vm.userCountyLabels = UserService.userCountyLabels;
-        vm.userFilteredCounty = UserService.userFilteredCounty;
-        vm.userSchoolLabels = UserService.userSchoolLabels;
-        vm.userFilteredSchool = UserService.userFilteredSchool;
-        vm.userPeopleServedLabels = UserService.userPeopleServedLabels;
-        vm.userFilteredPeopleServed = UserService.userFilteredPeopleServed;
-        vm.userAgeLabels = UserService.userAgeLabels;
-        vm.userFilteredAge = UserService.userFilteredAge;
+      //User Selected Year Charts 
+      vm.userCaseTypeLabels = UserService.userCaseTypeLabels;
+      vm.userFilteredCases = UserService.userFilteredCases;
+      vm.userCountyLabels = UserService.userCountyLabels;
+      vm.userFilteredCounty = UserService.userFilteredCounty;
+      vm.userSchoolLabels = UserService.userSchoolLabels;
+      vm.userFilteredSchool = UserService.userFilteredSchool;
+      vm.userPeopleServedLabels = UserService.userPeopleServedLabels;
+      vm.userFilteredPeopleServed = UserService.userFilteredPeopleServed;
+      vm.userAgeLabels = UserService.userAgeLabels;
+      vm.userFilteredAge = UserService.userFilteredAge;
 
-        vm.userVulnerabilityLabels = UserService.userVulnerabilityLabels;
-        vm.userFilteredVulnerability = UserService.userFilteredVulnerability;
+      vm.userVulnerabilityLabels = UserService.userVulnerabilityLabels;
+      vm.userFilteredVulnerability = UserService.userFilteredVulnerability;
+      vm.userLawLabels = UserService.userLawLabels;
+      vm.userFilteredLaw = UserService.userFilteredLaw;
+      vm.userRaceLabels = UserService.userRaceLabels;
+      vm.userFilteredRace = UserService.userFilteredRace;
 
-        var ctc = document.getElementById("userCaseTypeChart");
-        var myChart = new Chart(ctc, {
-          type: 'bar',
-          data: {
-            labels: vm.userCaseTypeLabels,
-            datasets: [{
-              label: 'Case Type',
-              backgroundColor: 'rgba(54, 162, 235, 0.2)',
-              borderColor: 'rgba(54, 162, 235, 0.2)',
-              data: vm.userFilteredCases,
-              borderWidth: 1
+      var ctc = document.getElementById("userCaseTypeChart");
+      var myChart = new Chart(ctc, {
+        type: 'bar',
+        data: {
+          labels: vm.userCaseTypeLabels,
+          datasets: [{
+            label: 'Case Type',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 0.2)',
+            data: vm.userFilteredCases,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                callback: function (value) { if (value % 1 === 0) { return value; } }
+              }
             }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true,
-                  callback: function (value) { if (value % 1 === 0) { return value; } }
-                }
-              }]
-            }
           }
+        }
       });
 
       var ctc = document.getElementById("userCountyChart");
@@ -72,124 +79,172 @@ myApp.controller('CoreController', function (UserService, $scope) {
             }]
           }
         }
-    });
+      });
 
-    var ctc = document.getElementById("userSchoolChart");
-    var myChart = new Chart(ctc, {
-      type: 'bar',
-      data: {
-        labels: vm.userSchoolLabels,
-        datasets: [{
-          label: 'School Districts',
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 0.2)',
-          data: vm.userFilteredSchool,
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true,
-              callback: function (value) { if (value % 1 === 0) { return value; } }
-            }
+      var ctc = document.getElementById("userSchoolChart");
+      var myChart = new Chart(ctc, {
+        type: 'bar',
+        data: {
+          labels: vm.userSchoolLabels,
+          datasets: [{
+            label: 'School Districts',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 0.2)',
+            data: vm.userFilteredSchool,
+            borderWidth: 1
           }]
-        }
-      }
-  });
-
-  var ctc = document.getElementById("userPeopleServedChart");
-  var myChart = new Chart(ctc, {
-    type: 'bar',
-    data: {
-      labels: vm.userPeopleServedLabels,
-      datasets: [{
-        label: 'People Served',
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 0.2)',
-        data: vm.userFilteredPeopleServed,
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            callback: function (value) { if (value % 1 === 0) { return value; } }
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                callback: function (value) { if (value % 1 === 0) { return value; } }
+              }
+            }]
           }
-        }]
-      }
-    }
-});
-
-var ctc = document.getElementById("userVulnerabilityChart");
-var myChart = new Chart(ctc, {
-  type: 'bar',
-  data: {
-    labels: vm.userVulnerabilityLabels,
-    datasets: [{
-      label: 'Vulnerability',
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      borderColor: 'rgba(54, 162, 235, 0.2)',
-      data: vm.userFilteredVulnerability,
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-          callback: function (value) { if (value % 1 === 0) { return value; } }
         }
-      }]
-    }
-  }
-});
+      });
 
-var ctc = document.getElementById("userAgeChart");
-var myChart = new Chart(ctc, {
-  type: 'bar',
-  data: {
-    labels: vm.userAgeLabels,
-    datasets: [{
-      label: 'Age',
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      borderColor: 'rgba(54, 162, 235, 0.2)',
-      data: vm.userFilteredAge,
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-          callback: function (value) { if (value % 1 === 0) { return value; } }
+      var ctc = document.getElementById("userPeopleServedChart");
+      var myChart = new Chart(ctc, {
+        type: 'bar',
+        data: {
+          labels: vm.userPeopleServedLabels,
+          datasets: [{
+            label: 'People Served',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 0.2)',
+            data: vm.userFilteredPeopleServed,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                callback: function (value) { if (value % 1 === 0) { return value; } }
+              }
+            }]
+          }
         }
-      }]
-    }
-  }
-});
+      });
+
+      var ctc = document.getElementById("userVulnerabilityChart");
+      var myChart = new Chart(ctc, {
+        type: 'bar',
+        data: {
+          labels: vm.userVulnerabilityLabels,
+          datasets: [{
+            label: 'Vulnerability',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 0.2)',
+            data: vm.userFilteredVulnerability,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                callback: function (value) { if (value % 1 === 0) { return value; } }
+              }
+            }]
+          }
+        }
+      });
+
+      var ctc = document.getElementById("userAgeChart");
+      var myChart = new Chart(ctc, {
+        type: 'bar',
+        data: {
+          labels: vm.userAgeLabels,
+          datasets: [{
+            label: 'Age',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 0.2)',
+            data: vm.userFilteredAge,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                callback: function (value) { if (value % 1 === 0) { return value; } }
+              }
+            }]
+          }
+        }
+      });
+
+      var ctc = document.getElementById("userLawEnforcementChart");
+      var myChart = new Chart(ctc, {
+        type: 'bar',
+        data: {
+          labels: vm.userLawLabels,
+          datasets: [{
+            label: 'Law Enforcement',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 0.2)',
+            data: vm.userFilteredLaw,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                callback: function (value) { if (value % 1 === 0) { return value; } }
+              }
+            }]
+          }
+        }
+      });
+
+      var ctc = document.getElementById("userRaceChart");
+      var myChart = new Chart(ctc, {
+        type: 'bar',
+        data: {
+          labels: vm.userRaceLabels,
+          datasets: [{
+            label: 'Race/ Ethnicity',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 0.2)',
+            data: vm.userFilteredRace,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                callback: function (value) { if (value % 1 === 0) { return value; } }
+              }
+            }]
+          }
+        }
+      });
 
     } else {
       return "Please select an Year";
     }
   };
 
-  Chart.defaults.scale.ticks.beginAtZero = true;
-
   vm.getChartData = function () {
     UserService.getChartData()
       .then(function () {
-        //GlobalCharts
+        //Global Charts
         vm.mainChartYears = UserService.mainChartYears;
         vm.filteredYears = UserService.filteredYears;
         vm.startCaseLabel = UserService.startCaseLabel;
         vm.filteredStartCase = UserService.filteredStartCase;
-        vm.referralLabel = UserService.referralLabel; 
+        vm.referralLabel = UserService.referralLabel;
         vm.filteredReferral = UserService.filteredReferral;
         vm.stateOverallLabel = UserService.stateOverallLabel;
         vm.filteredStateOverall = UserService.filteredStateOverall;
@@ -378,7 +433,7 @@ var myChart = new Chart(ctc, {
             }
           }
         });
-      
+
         var ctc = document.getElementById("genderOverallChart");
         var myChart = new Chart(ctc, {
           type: 'bar',
@@ -430,95 +485,95 @@ var myChart = new Chart(ctc, {
         });
       });
 
-  vm.getJoinTableData = function () {
-    UserService.getJoinTableData()
-      .then(function () {
-        vm.vulnerabilitiesOverallLabel = UserService.vulnerabilitiesOverallLabel;
-        vm.filteredVulnerabilitiesOverall = UserService.filteredVulnerabilitiesOverall;
-        vm.lawEnforcementOverallLabel = UserService.lawEnforcementOverallLabel;
-        vm.filteredLawEnforcementOverall = UserService.filteredLawEnforcementOverall;
-        vm.raceEthnicityOverallLabel = UserService.raceEthnicityOverallLabel;
-        vm.filteredRaceEthnicityOverall = UserService.filteredRaceEthnicityOverall;
+    vm.getJoinTableData = function () {
+      UserService.getJoinTableData()
+        .then(function () {
+          vm.vulnerabilitiesOverallLabel = UserService.vulnerabilitiesOverallLabel;
+          vm.filteredVulnerabilitiesOverall = UserService.filteredVulnerabilitiesOverall;
+          vm.lawEnforcementOverallLabel = UserService.lawEnforcementOverallLabel;
+          vm.filteredLawEnforcementOverall = UserService.filteredLawEnforcementOverall;
+          vm.raceEthnicityOverallLabel = UserService.raceEthnicityOverallLabel;
+          vm.filteredRaceEthnicityOverall = UserService.filteredRaceEthnicityOverall;
 
-        var ctc = document.getElementById("vulnerabilitiesOverallChart");
-        var myChart = new Chart(ctc, {
-          type: 'bar',
-          data: {
-            labels: vm.vulnerabilitiesOverallLabel,
-            datasets: [{
-              label: 'Overall Vulnerabilities',
-              backgroundColor: 'rgba(54, 162, 235, 0.2)',
-              borderColor: 'rgba(54, 162, 235, 0.2)',
-              data: vm.filteredVulnerabilitiesOverall,
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true,
-                  callback: function (value) { if (value % 1 === 0) { return value; } }
-                }
+          var ctc = document.getElementById("vulnerabilitiesOverallChart");
+          var myChart = new Chart(ctc, {
+            type: 'bar',
+            data: {
+              labels: vm.vulnerabilitiesOverallLabel,
+              datasets: [{
+                label: 'Overall Vulnerabilities',
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 0.2)',
+                data: vm.filteredVulnerabilitiesOverall,
+                borderWidth: 1
               }]
+            },
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true,
+                    callback: function (value) { if (value % 1 === 0) { return value; } }
+                  }
+                }]
+              }
             }
-          }
+          });
+
+          var ctc = document.getElementById("lawEnforcementOverallChart");
+          var myChart = new Chart(ctc, {
+            type: 'bar',
+            data: {
+              labels: vm.lawEnforcementOverallLabel,
+              datasets: [{
+                label: 'Overall Law Enforcement Agencies worked with',
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 0.2)',
+                data: vm.filteredLawEnforcementOverall,
+                borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true,
+                    callback: function (value) { if (value % 1 === 0) { return value; } }
+                  }
+                }]
+              }
+            }
+          });
+
+          var ctc = document.getElementById("raceEthnicityOverallChart");
+          var myChart = new Chart(ctc, {
+            type: 'bar',
+            data: {
+              labels: vm.raceEthnicityOverallLabel,
+              datasets: [{
+                label: 'Number of Cases by Race/Ethnicity',
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 0.2)',
+                data: vm.filteredRaceEthnicityOverall,
+                borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true,
+                    callback: function (value) { if (value % 1 === 0) { return value; } }
+                  }
+                }]
+              }
+            }
+          });
+
         });
 
-        var ctc = document.getElementById("lawEnforcementOverallChart");
-        var myChart = new Chart(ctc, {
-          type: 'bar',
-          data: {
-            labels: vm.lawEnforcementOverallLabel,
-            datasets: [{
-              label: 'Overall Law Enforcement Agencies worked with',
-              backgroundColor: 'rgba(54, 162, 235, 0.2)',
-              borderColor: 'rgba(54, 162, 235, 0.2)',
-              data: vm.filteredLawEnforcementOverall,
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true,
-                  callback: function (value) { if (value % 1 === 0) { return value; } }
-                }
-              }]
-            }
-          }
-        });
 
-        var ctc = document.getElementById("raceEthnicityOverallChart");
-        var myChart = new Chart(ctc, {
-          type: 'bar',
-          data: {
-            labels: vm.raceEthnicityOverallLabel,
-            datasets: [{
-              label: 'Number of Cases by Race/Ethnicity',
-              backgroundColor: 'rgba(54, 162, 235, 0.2)',
-              borderColor: 'rgba(54, 162, 235, 0.2)',
-              data: vm.filteredRaceEthnicityOverall,
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero: true,
-                  callback: function (value) { if (value % 1 === 0) { return value; } }
-                }
-              }]
-            }
-          }
-        });
-
-      });
-
+    }
 
   }
-
-}
 })
