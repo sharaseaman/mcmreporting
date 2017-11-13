93f652cab666774354edbdcb4a959688d9a24f21
Jessica Brown <jessicakbrown1120@gmail.com>
2017-11-13 12:02:23 -0600

    hm

--- server/public/scripts/controllers/new.controller.js
+++ server/public/scripts/controllers/new.controller.js
@@ -1,13 +1,74 @@
 myApp.controller('NewController', function (UserService) {
     console.log('NewController as nwc created');
     var vm = this;
+    var newIntake = [];
+  
 
-
-
+    vm.itemChange = function(){
+      console.log('inside itemchange');
+      console.log(vm.SchoolDistrictWhereChildWasEnrolledIn);
+    }
 
 
     vm.userService = UserService;
     vm.userObject = UserService.userObject;
+
+
+   UserService.getCities().then(function (response){
+    console.log('cities', response.data);
+    return vm.cities = response.data;
+  });
+
+  UserService.getCounties().then(function (response){
+    return vm.counties = response.data;
+  });
+
+  UserService.getAgencies().then(function (response){
+    return vm.agencies = response.data;
+  });
+
+  UserService.getSchools().then(function (response){
+    console.log('schools', response.data);
+    
+    return vm.schools = response.data;
+  });
+
+  
+  vm.click = function () {
+    console.log('in click');
+
+    var newIntake = {
+      mcm_number: vm.caseIn,
+      intake_date: vm.DateofIntaketoMCMIn,
+      age: vm.AgeIn,
+      gender: vm.GenderIn,
+      last_seen: vm.DateLastSeenIn,      
+      reported_missing: vm.DateReportedMissingtoPoliceIn,
+      people_served: vm.FamilyMembersInvolvedInSearchIn,      
+      city: vm.CityMissingFromIn,
+      county: vm.CountyMissingFromIn,
+      state: vm.StateMissingFromIn,
+      school: vm.SchoolDistrictWhereChildWasEnrolledIn,
+      start_case_type: vm.CaseTypeWhenOpenedIn,
+      end_case_type: vm.CaseTypeWhenClosedIn,
+      disposition: vm.CaseDispositionIn,
+      close_date: vm.DateCaseClosedIn,
+      referral_type: vm.ReferralTypeIn
+    };
+
+    console.log('newIntake', newIntake);
+    swal({
+        title: 'Required fields will be sent to database',
+        width: 600,
+        padding: 100,
+        background: '#fff url(assets/page.JPG)'
+    }).then(function () {
+        UserService.postInputData(newIntake);
+        // $location.path('/forms/newIntake');
+    });
+};
+
+
 
     //shara working on pdf below
     var pdf = new jsPDF();
