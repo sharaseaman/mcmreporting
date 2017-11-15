c47d061a911b809afbe4e4eb723eb8db174de4bd
Jessica Brown <jessicakbrown1120@gmail.com>
2017-11-14 21:25:48 -0600

    Select all from case_data working

--- server/routes/charts.router.js
+++ server/routes/charts.router.js
@@ -29,7 +29,6 @@ router.get('/', function (req, res) {
   }
 });
 
-// Handles Ajax request for user information if user is authenticated
 router.get('/join_tables_reports', function (req, res) {
   // check if logged in
   if (req.isAuthenticated()) {
@@ -55,91 +54,99 @@ router.get('/join_tables_reports', function (req, res) {
   }
 });
 
-//get for custom tables
-router.get('/custom', function (req, res) {
-  var custom = req.body;
-  console.log('In Get for custom info', custom);
-  // // check if logged in
-  // if (req.isAuthenticated()) {
-  //   pool.connect(function (conErr, client, done) {
-  //     if (conErr) {
-  //       res.sendStatus(500);
-  //     } else {
-  //       var queryArray = []
-  //       var valueArray = []
-  //       var tempArray = []
-  //       if (custom.start_case_type !== undefined) {
-  //         var start_case_type = 'WHERE start_case_type = $'
-  //         tempArray.push(start_case_type);
-  //         valueArray.push(custom.start_case_type);
-  //       }
-  //       if (custom.state !== undefined) {
-  //         var state = 'WHERE age = $'
-  //         tempArray.push(state);
-  //         valueArray.push(custom.state);
-  //       }
-  //       if (custom.county_name !== undefined) {
-  //         var county_name = '(SELECT id FROM counties WHERE county = $'
-  //         tempArray.push(county_name);
-  //         valueArray.push(gender.county_name);
-  //       }
-  //       if (custom.school_name !== undefined) {
-  //         var school_name = '(SELECT id FROM schools WHERE school = $'
-  //         tempArray.push(school_name);
-  //         valueArray.push(custom.school_name);
-  //       }
-  //       if (custom.age !== undefined) {
-  //         var age = 'WHERE age = $'
-  //         tempArray.push(age);
-  //         valueArray.push(custom.age);
-  //       }
-  //       if (custom.gender !== undefined) {
-  //         var gender = 'WHERE gender = $'
-  //         tempArray.push(gender);
-  //         valueArray.push(custom.gender);
-  //       }
-  //       if (custom.referral_type !== undefined) {
-  //         var referral_type = 'WHERE referral_type = $'
-  //         tempArray.push(referral_type);
-  //         valueArray.push(referral_type);
-  //       } // end list of queries
-
-  //       createQuery();
-  //     } //end pool else
-  //   }) //end pool.connect
-  // } else {
-  //   console.log('not logged in');
-  //   res.send(false);//end if pool
-  }) //end get custom charts - pairs w 59
-
-  //_____________________
-
-//   var createQuery = function () {
-//     tempArray.forEach(function (currentValue, index, array) {
-//       if (currentValue == '(SELECT id FROM counties WHERE county = $' || '(SELECT id FROM schools WHERE school = $') {
-//         count++
-//         queryArray.push(currentValue + count + ')')
-//         console.log('this is the if statement')
-//       } else {
-//         count++
-//         queryArray.push(currentValue + count)
-//         console.log('this is the else statement')
-//       } //end else 
-//       var query = queryArray.join(', ');
-//       console.log('query', query);
-//       var sqlQuery = 'SELECT * FROM case_data' + query
-//       client.query(sqlQuery, valueArray, function (queryErr, resultObj) {
-//         done();
-//         if (queryErr) {
-//           res.sendStatus(500);
-//         } else {
-//           res.send(resultObj.rows);
-//         } //if/else
-//       }) //client query
-//     }) //end tempArray.forEach
-//   }; //end create query
-
-// })
+//post for custom tables
+router.post('/custom', function (req, res) {
+  var custom = req.body
+  console.log('In Get for custom info', custom, custom.state);
+  // check if logged in
+  if (req.isAuthenticated()) {
+    pool.connect(function (conErr, client, done) {
+      if (conErr) {
+        res.sendStatus(500);
+      } else {
+        var queryArray = []
+        var valueArray = []
+        var tempArray = []
+        var count = 0
+        if (custom.start_case_type !== undefined) {
+          var start_case_type$ = 'start_case_type = $'
+          tempArray.push(start_case_type$);
+          valueArray.push(custom.start_case_type);
+        }
+        if (custom.state !== undefined) {
+          var state$ = 'state = $'
+          tempArray.push(state$)
+          valueArray.push(custom.state)
+          console.log('in if statement', custom.state, tempArray, valueArray)
+        }
+        if (custom.county_name !== undefined) {
+          var county_name$ = '(SELECT id FROM counties WHERE county = $'
+          tempArray.push(county_name$);
+          valueArray.push(custom.county_name);
+        }
+        if (custom.school_name !== undefined) {
+          var school_name$ = '(SELECT id FROM schools WHERE school = $'
+          tempArray.push(school_name$);
+          valueArray.push(custom.school_name);
+        }
+        if (custom.age !== undefined) {
+          var age$ = 'WHERE age = $'
+          tempArray.push(age$);
+          valueArray.push(custom.age);
+        }
+        if (custom.gender !== undefined) {
+          var gender$ = 'WHERE gender = $'
+          tempArray.push(gender$);
+          valueArray.push(custom.gender);
+        }
+        if (custom.referral_type !== undefined) {
+          var referral_type$ = 'WHERE referral_type = $'
+          tempArray.push(referral_type$);
+          valueArray.push(custom.referral_type);
+        } // end list of queries
+        // console.log('after if statement', state, tempArray)
+        console.log('valueArray', valueArray)
+        console.log('tmpArray', tempArray)
+        tempArray.forEach(function (currentValue, index, array) {
+          console.log('line 113, currentValue', currentValue)
+          if (currentValue === '(SELECT id FROM counties WHERE county = $') {
+            count++
+            queryArray.push(currentValue + count + ')')
+            console.log('this is the if statement')
+          } 
+          if (currentValue === '(SELECT id FROM schools WHERE school = $') {
+              count++
+              queryArray.push(currentValue + count + ')')
+              console.log('this is the second if statement')
+          } else {
+            count++
+            queryArray.push(currentValue + count)
+            console.log('this is the else statement')
+          } //end else 
+        }) //end tempArray.forEach
+          var query = queryArray.join(' AND ');
+          console.log('query', query);
+          var sqlQuery = 'SELECT * FROM case_data WHERE ' + query        
+          console.log('in SQL', sqlQuery)
+          console.log('in SQL values', valueArray)
+        
+          client.query(sqlQuery, valueArray, function (queryErr, resultObj) {
+            done();
+            console.log('resultObj', resultObj)
+            if (queryErr) {
+              res.sendStatus(500);
+            } else {
+              res.send(resultObj.rows);
+            } //if/else
+          }) //client query
+       
+      } //end pool else
+    }) //end pool.connect
+  } else {
+    console.log('not logged in');
+    res.send(false);//end if pool
+  } //end get custom charts - pairs w 59
+})
 
 module.exports = router;
 
